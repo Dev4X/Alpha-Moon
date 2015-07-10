32eeff6f795559a902a575da6875582905b83c0c
Hiren Dave <hdave10@gmail.com>
2015-07-10 18:54:52 +0530

    Display First EGRA Node based on Skills Map
    Storing node information in database and display it on home view.

--- gen/com/moonshot/dev4x/R.java
+++ gen/com/moonshot/dev4x/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int abc120=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
         public static final int contentHolder=0x7f060002;
+        public static final int homeContainer=0x7f060003;
         public static final int imgLogo=0x7f060001;
         public static final int topToolBar=0x7f060000;
     }
